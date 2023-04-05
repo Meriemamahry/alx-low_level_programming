45629b1bc83aa23f58295f5994a4059bd5f62669
Meriemamahry <maryemamahry2001@gmail.com>
2023-04-05 11:14:09 -0700

    print string

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,5 +1,7 @@
 #ifndef MAIN_H
 # define MAIN_H
+# include <stdio.h>
+# include <stdlib.h>
 
 int _putchar(char c);
 void _puts_recursion(char *s);
@@ -9,4 +11,5 @@ int factorial (int n);
 int _pow_recursion(int x,int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
+
 #endif
