ac5efc26b1cddb0d3c57287964fcd1ea7af52fb1
Meriemamahry <maryemamahry2001@gmail.com>
2023-04-07 10:15:40 -0700

    task0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <main.h>
+#include <stdio.h>
+/**
+*main - print the name of the program
+*@argc: number of command line argument
+*@argv: argument verctor
+*Rreturn: return 0
+*/
+void main(int argc, char* argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+	return (0);
+}
