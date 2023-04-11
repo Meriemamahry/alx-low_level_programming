cfa8bcce4dd39b92f4d79eb7d47abeb78eeb33dc
Meriemamahry <maryemamahry2001@gmail.com>
2023-04-11 07:11:13 -0700

    s

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,39 +2,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * str_concat -  returns a pointer to a 2 dimensional array of integers
- * @s1: width of the grid
- * @s2: height of the grid
- * Return: Nothing
+ * _strdup - check the code for ALX School students.
+ * @str: input
+ * Return: Always 0
  */
-char *str_concat(char *s1, char *s2)
+char *_strdup(char *str)
 {
-unsigned int size1 = 0, size2 = 0;
-char *ptr, *ret;
-ptr = s1;
-if (s1)
+char *nstr;
+unsigned int len, i;
+if (str == NULL)
 {
-while (*ptr++)
-size1++;
-}
-else
-s1 = "";
-ptr = s2;
-if (s2)
-{
-while (*ptr++)
-size2++;
-}
-else
-s2 = "";
-ret = malloc(size1 + size2 + 1);
-if (!ret)
 return (NULL);
-ptr = ret;
-while (*s1)
-*ptr++ = *s1++;
-while (*s2)
-*ptr++ = *s2++;
-*ptr = 0;
-return (ret);
+}
+len = 0;
+while (str[len] != '\0')
+{
+len++;
+}
+nstr = malloc(sizeof(char) * (len + 1));
+if (nstr == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < len; i++)
+{
+nstr[i] = str[i];
+}
+nstr[len] = '\0';
+return (nstr);
 }
