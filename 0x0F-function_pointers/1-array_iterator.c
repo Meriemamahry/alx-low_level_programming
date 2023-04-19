5b0008ffdfc15d12ea314eb06ce631ae6ccfb5df
Meriemamahry <maryemamahry2001@gmail.com>
2023-04-19 16:32:55 -0700

    task1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - executes a function as parameter
+ * @array: array
+ * @size: size
+ * @action: pointer to a function
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (!action || !array)
+		return;
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
