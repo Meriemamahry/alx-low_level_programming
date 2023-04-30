cf94164d01d3e3c36ff753e1e88996b7aa47755e
Meriemamahry <maryemamahry2001@gmail.com>
2023-04-30 09:12:44 -0700

    task7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -5,25 +5,19 @@
  * a struct list type listint_t
  * @head: pointer to the head of listint_s
  * @index: index of node in list, starting at 0
- * Return: address of index node of listint_t 
+ * Return: address of index node of listint_t
  */
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *tmp = head;
-	unsigned int i;
+	int i = 0;
 
-	if (!head)
-	{
+	if (head == NULL)
 		return (NULL);
-	}
-	for (i = 0; head->next; i++)
+	while (i < index)
 	{
-		if (i == index)
-		{
-			return (tmp);
-		}
-		tmp = tmp->next;
+		head = head->next;
+		i++;
 	}
-	return (NULL);
+	return (head);
 }
